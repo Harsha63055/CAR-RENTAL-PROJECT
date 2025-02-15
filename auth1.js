2f9ed2198445389e137d2b59af6b47241518f3a6
HarshaVardhan Developer <harshavardhan96310@gmail.com>
2025-02-15 17:05:18 +0530

    updates

--- auth1.js
+++ auth1.js
@@ -1,31 +1,17 @@
-import {
-  auth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "./firebase.js";
+import { auth, createUserWithEmailAndPassword } from "./firebase.js";
 
-let signup = document.getElementById("Signup");
-
-signup.addEventListener("submit", (e) => {
+document.getElementById("signup").addEventListener("submit", (e) => {
   e.preventDefault();
-  let email = e.target[0].value;
-  let password = e.target[1].value;
-  // let retypePassword = e.target[2].value;
 
-  // if(password !== retypePassword) {
-  //   alert("Passwords do not match!");
-  //   return;
-  // }
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
 
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      console.log("User signed up:", userCredential.user);
       alert("Signup successful! Redirecting to login...");
-
-      window.location.href = "./admin.html";
+      window.location.href = "./index1.html";
     })
     .catch((error) => {
-      console.error("Error:", error.message);
       alert(error.message);
     });
 });
--- auth4.js
+++ auth4.js
@@ -1,32 +1,17 @@
-import {
-    auth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-  } from "./firebase.js";
-  
-  let signup = document.getElementById("Userlogin");
-  
-  signup.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let email = e.target[0].value;
-    let password = e.target[1].value;
-    // let retypePassword = e.target[2].value;
-  
-    // if(password !== retypePassword) {
-    //   alert("Passwords do not match!");
-    //   return;
-    // }
-  
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        console.log("User signed up:", userCredential.user);
-        alert("Signin successful! Redirecting to Home page...");
-  
-        window.location.href = "../USER/home.html";
-      })
-      .catch((error) => {
-        console.error("Error:", error.message);
-        alert(error.message);
-      });
-  });
-  
+import { auth, signInWithEmailAndPassword } from "./firebase.js";
+
+document.getElementById("Userlogin").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      alert("Login successful! Redirecting to Home...");
+      window.location.href = "./USER/home.html";
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+});
